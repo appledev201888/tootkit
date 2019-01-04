ede9c73ffaafb8fd26ba54cfcb7341c9ae087be9
chenweibin <weibinchen@uutaka.com>
2019-01-04 19:02:03 +0800

    提交的描述说明

--- Classes/CXYToolKits.framework/Headers/CXYToolKitsManage.h
+++ Classes/CXYToolKits.framework/Headers/CXYToolKitsManage.h
@@ -1,12 +1,17 @@
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CXYToolKitsManage : NSObject
 
-+ (void)CXYToolKitsManageWithAid:(NSString *)aid
-                               k:(NSString *)k;
++ (CXYToolKitsManage *)sharedManager;
+
+- (void)CXYToolKitsbegin:(UIView *)view;
+
+- (void)CXYToolKitsend:(UIView *)view;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- tootkit.podspec
+++ tootkit.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "tootkit"
-  s.version      = "0.0.5"
+  s.version      = "0.0.6"
   s.summary      = "我的自定义库，目前是测试版，里面啥都没有！"
 
   # This description is used to generate tags and improve search results.
